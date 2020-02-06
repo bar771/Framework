369c9d98dfeed38442827ba831f555fe5a480da7
Laptop\Bar <bar4477@gmail.com>
2020-02-06 15:44:55 +0200

    'UpdateTwo'

--- application/models/user.php
+++ application/models/user.php
@@ -16,10 +16,7 @@ class User extends baseObject {
 	public function hasPrivilege() {
 		$funcs = func_get_args();
 
-		$stmt = $this->database->prepare('SELECT * FROM users WHERE id=:id');
-		$stmt->bindParam(':id', $this->getUserID());
-		$stmt->closeCursor();
-		$stmt->execute();
+		$stmt = $this->database->prepare('SELECT * FROM users WHERE id=?', array($this->getUserID()));
 		$user = $stmt->fetch();
 		foreach ($funcs as $func)
 			if ($user['privilege'] == $func)
@@ -33,9 +30,7 @@ class User extends baseObject {
 		if ($id == 0) return 'Guest';
 		if (!preg_match('/^[0-9]$/', $id)) return 'Unknown';
 
-		$stmt = $this->database->prepare('SELECT username FROM users WHERE id=:id');
-		$stmt->bindParam(':id', $id);
-		$stmt->execute();
+		$stmt = $this->database->prepare('SELECT username FROM users WHERE id=?', array($id));
 		$username = $stmt->fetch()['username'];
 		return $username;
 	}
@@ -43,9 +38,7 @@ class User extends baseObject {
 	public function get_id_by_username($username) {
 		if (!preg_match('/^[a-z][A-Z][!@#$%^&*()_+\\\[\]\'"]$/', $id)) return null;
 
-		$stmt = $this->database->prepare('SELECT id FROM users WHERE username=:username');
-		$stmt->bindParam(':username', $username);
-		$stmt->execute();
+		$stmt = $this->database->prepare('SELECT id FROM users WHERE username=?', $username);
 		$id = $stmt->fetch()['id'];
 		return $id;
 	}
@@ -53,30 +46,20 @@ class User extends baseObject {
 	public function getUserID() {
 		if (!isset($_SESSION['AUTHSSES'])) return 0;
 
-		$sess = $this->database->prepare('SELECT COUNT(*) AS count FROM user_sessions WHERE hash=:hash');
-		$sess->bindParam(':hash', $_SESSION['AUTHSSES']);
-		$sess->closeCursor();
-		$sess->execute();
+		$sess = $this->database->prepare('SELECT COUNT(*) AS count FROM user_sessions WHERE hash=?', array($_SESSION['AUTHSSES']));
 		$count = $sess->fetch()['count'];
 
-		$stmt = $this->database->prepare('SELECT username FROM user_sessions WHERE hash=:hash');
-		$stmt->bindParam(':hash', $_SESSION['AUTHSSES']);
-		$stmt->closeCursor();
-		$stmt->execute();
+		$stmt = $this->database->prepare('SELECT username FROM user_sessions WHERE hash=?', array($_SESSION['AUTHSSES']));
 		$username = $stmt->fetch()['username'];
 
-		$user = $this->database->prepare('SELECT id FROM users WHERE username=:username');
-		$user->bindParam(':username', $username);
-		$user->execute();
+		$user = $this->database->prepare('SELECT id FROM users WHERE username=:username', array($username));
 		$id = $user->fetch()['id'];
 		return ($count > 0 ? $id : 0);
 	}
 
 	public function logoutUser() {
 		if (!isset($_SESSION['AUTHSSES'])) return false;
-		$stmt = $this->database->prepare('DELETE FROM user_sessions WHERE hash=:hash AND username=:uname');
-		$stmt->bindValue(':hash', $_SESSION['AUTHSSES']);
-		$stmt->bindValue(':uname', $this->getUsernameByID($this->getUserID()));
+		$stmt = $this->database->prepare('DELETE FROM user_sessions WHERE hash=? AND username=?', array($_SESSION['AUTHSSES'], $this->getUsernameByID($this->getUserID())));
 		$stmt->execute();
 		return true;
 	}
@@ -84,21 +67,14 @@ class User extends baseObject {
 	public function blockUser($daysCount = 1) {
 		if (!isset($_SESSION['AUTHSSES'])) return false;
 
-		$stmt = $this->database->prepare('UPDATE users SET isBlocked=:block, blockExpiry=:exp');
-		$stmt->bindValue(':block', 1);
-		$stmt->bindValue(':exp', time()*60*60*24*$daysCount);
-		$stmt->closeCursor();
-		$stmt->execute();
+		$stmt = $this->database->prepare('UPDATE users SET isBlocked=?, blockExpiry=?', array(1, time()*60*60*24*$daysCount));
 		return true;
 	}
 
 	public function isUserBlocked($userID) {
 		if (!isset($_SESSION['AUTHSSES'])) return false;
 
-		$stmt = $this->database->prepare('SELECT isBlocked, blockExpiry FROM users WHERE userID=:id');
-		$stmt->bindValue(':id', $userID);
-		$stmt->closeCursor();
-		$stmt->execute();
+		$stmt = $this->database->prepare('SELECT isBlocked, blockExpiry FROM users WHERE userID=?', array($userID));
 		$row = $stmt->fetch();
 		return array('block'=>$row['isBlocked'], 'exp'=>$row['blockExpiry']);
 	}
@@ -106,10 +82,7 @@ class User extends baseObject {
 	public function isOnline() {
 		if (!isset($_SESSION['AUTHSSES'])) return false;
 
-		$sess = $this->database->prepare('SELECT COUNT(*) AS count FROM user_sessions WHERE hash=:hash');
-		$sess->bindParam(':hash', $_SESSION['AUTHSSES']);
-		$sess->closeCursor(); // ??
-		$sess->execute();
+		$sess = $this->database->prepare('SELECT COUNT(*) AS count FROM user_sessions WHERE hash=?', array($_SESSION['AUTHSSES']));
 		$row = $sess->fetch();
 		return ($row['count'] > 0 ? true : false);
 	}
@@ -117,17 +90,10 @@ class User extends baseObject {
 	public function authenticateSession() {
 		if (!isset($_SESSION['AUTHSSES'])) return false;
 		$session = $this->protect_xss($_SESSION['AUTHSSES']);
-		//$sess = $this->database->getOne('SELECT * FROM user_sessions WHERE hash=?s', $session);
-		$sess = $this->database->prepare('SELECT COUNT(*) AS count FROM user_sessions WHERE hash=:hash');
-		$sess->bindParam(':hash', $session);
-		$sess->closeCursor(); // ??
-		$sess->execute();
+		$sess = $this->database->prepare('SELECT COUNT(*) AS count FROM user_sessions WHERE hash=?', array($session));
 		$count = $sess->fetch()['count'];
 
-		$stmt = $this->database->prepare('SELECT ip FROM user_sessions WHERE hash=:hash');
-		$stmt->bindParam(':hash', $session);
-		$stmt->closeCursor();
-		$stmt->execute();
+		$stmt = $this->database->prepare('SELECT ip FROM user_sessions WHERE hash=?', array($session));
 		$ipAddress = $stmt->fetch()['ip'];
 
 		return ($count > 0 ? ($ipAddress == $_SERVER['REMOTE_ADDR'] ? true : false) : false);
@@ -140,43 +106,13 @@ class User extends baseObject {
 		srand(time());
 		$id = rand(1000, 9999);
 
-		$stmt = $this->database->prepare('INSERT INTO user_sessions SET id=:id, username=:username, hash=:hash, ip=:ip, country=:country, time=:time');
-		$stmt->bindParam(':id', $id);
-		$stmt->bindParam(':username', $username);
-		$stmt->bindParam(':hash', $hash);
-		$stmt->bindValue(':ip', $_SERVER['REMOTE_ADDR']);
-		$stmt->bindParam(':country', $country);
-		$stmt->bindValue(':time', time());
-		$stmt->closeCursor(); // ??
+		$stmt = $this->database->prepare('INSERT INTO user_sessions SET id=?, username=?, hash=?, ip=?, country=?, time=?', array($id, $username, $hash, $_SERVER['REMOTE_ADDR'], $country, time()));
 
 		if ($stmt->execute()) {
 			$_SESSION['AUTHSSES'] = $hash;
 			return true;
 		}
 		return false;
-	}
-
-	public function editProfile($val = array()) {
-		$username = $val['username'];
-		$password = $val['password'];
-		$cpassword = $val['cpassword'];
-		$email = $val['email'];
-		$profileImage = $val['pimage'];
-
-		if (!empty($username)) {
-			// Check if username is available.
-			// Change username.
-		}
-		if (!empty($password) && !empty($cpassword)) {
-			// Change password.
-		}
-		if (!empty($email)) {
-			// Check if email is available.
-			// Change email.
-		}
-		if (!empty($profileImage)) {
-			// Delete previous image and update with new one.
-		}
 	}
 
 	public function doLogin($val = array())
@@ -219,22 +155,6 @@ class User extends baseObject {
 		$stmt->closeCursor(); // ??
 		return $stmt->execute();
 	}
-
-	/*
-	 * @return array
-	**/
-	public function getUserRow() {
-		if (!$this->isOnline() || !$this->authenticateSession())
-			return null;
-
-			$stmt = $this->pdo->prepare('SELECT * FROM users WHERE id=:id');
-			$stmt->bindParam(':id', $this->getUserID());
-			$stmt->closeCursor();
-			$stmt->execute();
-			return $stmt->fetch();
-	}
-
 }
-
 
 ?>
--- application/views/index.php
+++ application/views/index.php
@@ -2,6 +2,13 @@
 
 		<div>
 			<?php echo WEBSITE_AUTHOR; ?> Framework <?php echo WEBSITE_VERSION; ?>
+
 		</div>
+<style type="text/css">
+/* Styling locally */
+</style>
+<script type="text/javasacript>
+// JS Script
+</script>
 
 <?php include "layouts/footer.php"; ?>
