ba584595a8098afb741c678b3b995dfe0c943ad9
ilCapo01 <ilCapo01@example>
2020-02-18 15:20:43 +0200

    PayPal

---
+++ application/libraries/PayPal/paypal.php
@@ -0,0 +1,22 @@
+<?php
+
+// https://developer.paypal.com/docs/ipn/
+// https://developer.paypal.com/docs/ipn/integration-guide/IPNImplementation/
+// https://developer.paypal.com/docs/ipn/integration-guide/ht-ipn/
+// https://developer.paypal.com/docs/ipn/integration-guide/IPNSetup/
+
+
+class PayPal {
+  /*
+   * Functions:
+   * Build payment form.
+   * Finalize payment with IPN- activate whatever you want to activate, or log the transaction in your db.
+  **/
+  function __construct()
+  {
+    // Constructor
+  }
+}
+
+
+?>
