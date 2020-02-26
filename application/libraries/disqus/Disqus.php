61c11fd8f060cb093c7f28836d8dfad3794fe45b
ilCapo01 <ilCapo01@example>
2020-02-26 14:31:49 +0200

    Disqus library updated

--- application/libraries/disqus/Disqus.php
+++ application/libraries/disqus/Disqus.php
@@ -2,13 +2,20 @@
 namespace Disqus;
 
 /**
+ ****************************
  * Disqus unofficial library
+ ****************************
+ *
+ * Disqus official docs:
  * https://disqus.com/api/docs/
+ *
+ * Forums:
+ * https://disqus.com/api/docs/forums/
  */
 class Disqus {
 
-  var $key;
-  var $secret;
+  var $key = '';
+  var $secret = '';
 
   function __construct($key, $secret) {
     $this->key = $key;
@@ -16,18 +23,36 @@ class Disqus {
   }
 
   // https://disqus.com/api/docs/requests/
-  function requestAPI($res = 'listPosts', $params = '') {
-    $url = 'https://disqus.com/api/3.0/'.$res.'.json?'.$params.'&api_key='.$key.'&api_secret='.$secret;
-    $data = null;
+  function requestAPI($res = 'listPosts', $getData = '', $postData = '') {
+    $url = 'https://disqus.com/api/3.0/'.$res.'.json?'.$getData.'&api_key='.$this->key.'&api_secret='.$this->secret;
 
-    return $this->sendRequest($url, $data);
+    return json_decode($this->sendRequest($url, $postData));
   }
 
-  function sendRequest($url, $data, $doUpload = false) {
-    if (!$doUpload) $data = http_build_query($data);
+  // https://disqus.com/api/docs/forums/listCategories/
+  function getForumCategories()
+  {
+    return $this->requestAPI('listCategories', '');
+  }
 
+  // https://disqus.com/api/docs/forums/listPosts/
+  function getForumPosts()
+  {
+    return $this->requestAPI('listPosts', '');
+  }
+
+  // https://disqus.com/api/docs/forums/listThreads/
+  function getForumThreads()
+  {
+    return $this->requestAPI('listThreads', '');
+  }
+
+  private function sendRequest($url, $data, $doUpload = false) {
+    if (!$doUpload) $data = http_build_query($data);
+    // Initialize cURL.
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
+    // POST parameters.
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-type: '.($doUpload ? 'multipart/form-data' : 'application/x-www-form-urlencoded'),
         'Content-length: '.sizeof($data)
@@ -35,18 +60,19 @@ class Disqus {
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Unofficial API library for Disqus.com 1.0'); // User agent
+    // User agent
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Unofficial API library for Disqus.com 1.0');
     // SSL Support.
     $cert = dirname(__FILE__) . '/cacert.pem';
     $ssl = (file_exists($cert) ? true : false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $ssl);
     curl_setopt($ch, CURLOPT_CAINFO, $cert);
     curl_setopt($ch, CURLOPT_CAPATH, $cert);
+    // Server response.
+    $res = curl_exec($ch);
+    curl_close($ch);
 
-    $server_output = curl_exec($ch);
-    curl_close ($ch);
-
-    return $server_output;
+    return $res;
   }
 }
 
